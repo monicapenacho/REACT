cb2d5998b404f1012f954db5fa2d101134b658c3
monicapenacho <monicapenachocollado@gmail.com>
2024-08-17 18:19:23 +0200

    CarouselImage

--- tiendapokemon/src/componentes/CarouselInterval.js
+++ tiendapokemon/src/componentes/CarouselInterval.js
@@ -1,19 +1,12 @@
 import Carousel from 'react-bootstrap/Carousel';
+import CarouselImage from './CarouselImage';
 
 function CarouselInterval() {
   return (  
     <Carousel className="carouselContainer">
       <Carousel.Item interval={1000}>
 
-            <img
-
-            className="d-block w-100 carouselImage"
-
-            src="/image/Bulbasaur.jpg"
-
-            alt="Bulbasaur"
-
-            />
+           <CarouselImage image="/image/Bulbasaur.jpg" text="Bulbasaur" />
 
         <Carousel.Caption className="carousel-caption">
           <h3>Bulbasaur</h3>
@@ -21,30 +14,16 @@ function CarouselInterval() {
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item interval={500}>
-            <img
 
-            className="d-block w-100 carouselImage"
-
-            src="/image/ivysaur.jpg"
-
-            alt="Ivysaur"
-
-            />
+            <CarouselImage image="/image/ivysaur.jpg" text="Ivysaur" />
+          
         <Carousel.Caption className="carousel-caption">
           <h3>Ivysau</h3>
           {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-            <img
-
-            className="d-block w-100 carouselImage"
-
-            src="/image/Venusaur.jpg"
-
-            alt="Venusaur"
-
-            />
+            <CarouselImage image="/image/Venusaur.jpg" text="Venusaur" />
         <Carousel.Caption className="carousel-caption">
           <h3>Venusaur</h3>
           {/* <p>
